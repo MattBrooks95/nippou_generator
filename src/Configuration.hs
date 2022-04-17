2fc8838f011d314e26a5c2629a44432db9b87732
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2022-04-17 19:34:31 +0900

    I was able to refactor this code so that the type of the environment variables in the configuration struct no longer needed to be IO Maybe String
    sequence was the key!

--- src/Configuration.hs
+++ src/Configuration.hs
@@ -33,44 +33,56 @@ appConfigDir = "nippou_generator"
 
 data NippouGeneratorConfig = NippouGeneratorConfig {
 		workCodes:: WorkCodes,
-		environmentVariables:: Map.Map String (IO (Maybe String))
-	}
+		environmentVariables:: Map.Map String (Maybe String)
+	} deriving Show
 
-instance Show NippouGeneratorConfig where
-	show nippouGeneratorConfig = intercalate "\n" [shownWorkCodes, shownEnvironmentVariableMap]
-		where
-			shownWorkCodes = show (workCodes nippouGeneratorConfig)
-			--shownEnvironmentVariableMap = intercalate " " (map (\x -> (fst x) ++ ":" ++ (fromMaybe "env variable not found" (snd x))) (toList (environmentVariables nippouGeneratorConfig)))
-			shownEnvironmentVariableMap = show "TODO print IO values in the environment variable map"
-			--shownEnvironmentVariableMap = show (map fromMaybe (environmentVariables nippouGeneratorConfig))
-			--shownEnvironmentVariableMap = show (environmentVariables nippouGeneratorConfig)
+--instance Show NippouGeneratorConfig where
+--	show nippouGeneratorConfig = intercalate "\n" [shownWorkCodes, shownEnvironmentVariableMap]
+--		where
+--			shownWorkCodes = show (workCodes nippouGeneratorConfig)
+--			--shownEnvironmentVariableMap = intercalate " " (map (\x -> (fst x) ++ ":" ++ (fromMaybe "env variable not found" (snd x))) (toList (environmentVariables nippouGeneratorConfig)))
+--			shownEnvironmentVariableMap = show "TODO print IO values in the environment variable map"
+--			--shownEnvironmentVariableMap = show (map fromMaybe (environmentVariables nippouGeneratorConfig))
+--			--shownEnvironmentVariableMap = show (environmentVariables nippouGeneratorConfig)
 
 defaultMaybe :: Maybe String -> String -> String
 defaultMaybe realString defaultString = fromMaybe defaultString realString
 
-getEnvironmentVariablesMap :: (String -> IO (Maybe String)) -> [String] -> Map.Map String (IO (Maybe String))
-getEnvironmentVariablesMap getVariable variableNames =
-	let variables = [ (x, getVariable x) | x <- variableNames ] in
-	Map.fromList variables
+--getEnvironmentVariablesMap :: (String -> IO (Maybe String)) -> [String] -> Map.Map String (IO (Maybe String))
+--getEnvironmentVariablesMap getVariable variableNames =
+--	let variables = [ (x, getVariable x) | x <- variableNames ] in
+--	Map.fromList variables
+
+getEnvironmentVariablesMap :: [(String, Maybe String)] -> Map.Map String (Maybe String)
+getEnvironmentVariablesMap variables = Map.fromList variables
 
 getConfiguration:: IO NippouGeneratorConfig
 getConfiguration = do
-	homeDirectory <- getEnvironmentVariable "HOME"
-	case homeDirectory of
-		Just homeDir -> print ("home dir:" ++ homeDir)
-		Nothing -> print "home dir was not found"
-	xdgConfigDir <- getEnvironmentVariable "XDG_CONFIG_HOME"
-	case xdgConfigDir of
-		Just configDir -> print $ "config dir:" ++ configDir
-		Nothing -> print "config dir was not found"
+	--homeDirectory <- getEnvironmentVariable "HOME"
+	--case homeDirectory of
+	--	Just homeDir -> print ("home dir:" ++ homeDir)
+	--	Nothing -> print "home dir was not found"
+	--xdgConfigDir <- getEnvironmentVariable "XDG_CONFIG_HOME"
+	--case xdgConfigDir of
+	--	Just configDir -> print $ "config dir:" ++ configDir
+	--	Nothing -> print "config dir was not found"
+	--environmentVariablesList <- sequence (map getEnvironmentVariable targetEnvironmentVariables)
+	environmentVariablesResults <- sequence (map getEnvironmentVariable targetEnvironmentVariables)
+	let environmentVariablesList = zip targetEnvironmentVariables environmentVariablesResults
+	let environmentVariablesMap = getEnvironmentVariablesMap environmentVariablesList
 	return NippouGeneratorConfig {
 			workCodes=[],
 			environmentVariables=environmentVariablesMap
 		}
-	where environmentVariablesMap = getEnvironmentVariablesMap getEnvironmentVariable [
-			"XDG_CONFIG_HOME",
-			"HOME",
-			"APPDATA"
-		]
+	
+	where
+		--environmentVariablesMap = getEnvironmentVariablesMap environmentVariablesList
+		--environmentVariablesMap = getEnvironmentVariablesMap getEnvironmentVariable targetEnvironmentVariables
+		targetEnvironmentVariables = [
+				"XDG_CONFIG_HOME",
+				"HOME",
+				"APPDATA"
+			]
+
 		
 
